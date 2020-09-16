288e9072ccff4601e654ff2c58884f24c8f6b470
hungphqu98 <pqh2405@gmail.com>
2020-09-16 12:05:33 +0700

    test

---
+++ quotes.js
@@ -0,0 +1,8 @@
+var quotes = [
+    {
+        quote: "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+        author: Anonymous,
+        topic: []
+    }
+
+]
